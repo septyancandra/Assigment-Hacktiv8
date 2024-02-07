fdf964463d4d4103ccc59644d61ee438a47c204a
septyancandra <73571160+septyancandra@users.noreply.github.com>
2024-02-07 17:02:20 +0700

    Delete Day8/swagger directory

--- Day8/swagger/models/car.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type Car struct {
-	ID       uint   `gorm:"primaryKey"`
-	Merk     string `gorm:"not null; type:varchar(191)"`
-	TypeCars string `gorm:"not null; type:varchar(191)"`
-	Harga    int    `gorm:"not null; type:int"`
-}
